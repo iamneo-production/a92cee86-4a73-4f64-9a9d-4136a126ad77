f202b1c700446f45840ae0398ac2828956064e06
Harika-17 <126892768+Harika-17@users.noreply.github.com>
2023-03-11 14:35:47 +0530

    Update views.py

--- base/demo/views.py
+++ base/demo/views.py
@@ -1,239 +1,86 @@
-# Create your views here.
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate,login
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from .models import Profile, Post, joblistings,jobboards, FollowersCount
-from itertools import chain
-import random
+# from django.http import HttpResponse
+# from django.contrib.auth.decorators import login_required
+# from itertools import chain
+# import random
 
-# Create your views here.
+from .forms import RegisterForm
 
-@login_required(login_url='signin')
 def index(request):
-    user_object = User.objects.get(username=request.user.username)
-    user_profile = Profile.objects.get(user=user_object)
-
-    user_following_list = []
-    feed = []
-
-    user_following = FollowersCount.objects.filter(follower=request.user.username)
-
-    for users in user_following:
-        user_following_list.append(users.user)
-
-    for usernames in user_following_list:
-        feed_lists = Post.objects.filter(user=usernames)
-        feed.append(feed_lists)
-
-    feed_list = list(chain(*feed))
-
-    # user suggestion starts
-    all_users = User.objects.all()
-    user_following_all = []
-
-    for user in user_following:
-        user_list = User.objects.get(username=user.user)
-        user_following_all.append(user_list)
-    
-    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
-    current_user = User.objects.filter(username=request.user.username)
-    final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
-    random.shuffle(final_suggestions_list)
-
-    username_profile = []
-    username_profile_list = []
-
-    for users in final_suggestions_list:
-        username_profile.append(users.id)
-
-    for ids in username_profile:
-        profile_lists = Profile.objects.filter(id_user=ids)
-        username_profile_list.append(profile_lists)
-
-    suggestions_username_profile_list = list(chain(*username_profile_list))
-
-
-    return render(request, 'index.html', {'user_profile': user_profile, 'posts':feed_list, 'suggestions_username_profile_list': suggestions_username_profile_list[:4]})
-
-@login_required(login_url='signin')
-def upload(request):
-
-    if request.method == 'POST':
-        user = request.user.username
-        image = request.FILES.get('image_upload')
-        caption = request.POST['caption']
-
-        new_post = Post.objects.create(user=user, image=image, caption=caption)
-        new_post.save()
-
-        return redirect('/')
+  if request.method=='POST':
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+    if username and password:
+      user=authenticate(username=username,password=password)
+      print(username,password)
+      print(user)
+      if user is not None:
+        login(request,user)
+        return redirect('home')
+      else:
+        messages.error(request,'Username or Password is Incorrect')
     else:
-        return redirect('/')
-
-@login_required(login_url='signin')
-def search(request):
-    user_object = User.objects.get(username=request.user.username)
-    user_profile = Profile.objects.get(user=user_object)
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        username_object = User.objects.filter(username__icontains=username)
-
-        username_profile = []
-        username_profile_list = []
-
-        for users in username_object:
-            username_profile.append(users.id)
-
-        for ids in username_profile:
-            profile_lists = Profile.objects.filter(id_user=ids)
-            username_profile_list.append(profile_lists)
-        
-        username_profile_list = list(chain(*username_profile_list))
-    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
-
-@login_required(login_url='signin')
-def joblistings(request):
-    joblistings=joblistings.objects.all()
-    return render(request,'joblistings.html',{'joblistings':joblistings})
+      messages.error(request,'Fill out all the fields')
+  
+  return render(request,'core/signin.html',{})
 
 
-@login_required(login_url='signin')
+def registerUser(request):
+  print("CCCCCCCCCCC1")
+  form=RegisterForm()
+  print(request.method)
+  if request.method=='POST':
+    print("CCCCCCCCCCCCCCC2")
+    form=RegisterForm(request.POST)
+    if form.is_valid():
+      print("CCCCCCCCCCCCCCCCCCCCCC3")
+      form.save()
+      # return redirect('signin')
+      return render(request,'core/signin.html',{})
+  # else:
+  #   form=RegisterForm()
+  return render(request,'core/signup.html',{'form':form})
+
+
+def loginUser(request):
+  # print("CCCCCCCCCCCCCCCCC1")
+  if request.method=='POST':
+    # print("CCCCCCCCCCCCCCCCC2")
+    username=request.POST.get('username')
+    password=request.POST.get('password')
+    # print("CCCCCCCCCCCCCCCCC3")
+    # print(emailaddress,password)
+    if username and password:
+      user=authenticate(username=username,password=password)
+      print(username,password)
+      print(user)
+      if user is not None:
+        login(request,user)
+        return redirect('home')
+      else:
+        messages.error(request,'Username or Password is Incorrect')
+    else:
+      messages.error(request,'Fill out all the fields')
+  
+  return render(request,'core/signin.html',{})
+
+def home(request):
+  return render(request,'core/homepage.html',{})
+
+def profiles(request):
+  return render(request,'core/profiles.html',{})
+
 def jobboards(request):
-    jobboards=jobboards.objects.all()
-    return render(request,'jobboards.html',{'jobboards':jobboards})
+  return render(request,'core/jobboards.html',{})
 
+def eventlistings(request):
+  return render(request,'core/eventlistings.html',{})
 
-@login_required(login_url='signin')
-def profile(request, pk):
-    user_object = User.objects.get(username=pk)
-    user_profile = Profile.objects.get(user=user_object)
-    user_posts = Post.objects.filter(user=pk)
-    user_post_length = len(user_posts)
+def joblisitings(request):
+  return render(request,'core/joblistings.html',{})
 
-    follower = request.user.username
-    user = pk
-
-    if FollowersCount.objects.filter(follower=follower, user=user).first():
-        button_text = 'Unfollow'
-    else:
-        button_text = 'Follow'
-
-    user_followers = len(FollowersCount.objects.filter(user=pk))
-    user_following = len(FollowersCount.objects.filter(follower=pk))
-
-    context = {
-        'user_object': user_object,
-        'user_profile': user_profile,
-        'user_posts': user_posts,
-        'user_post_length': user_post_length,
-        'button_text': button_text,
-        'user_followers': user_followers,
-        'user_following': user_following,
-    }
-    return render(request, 'profile.html', context)
-
-@login_required(login_url='signin')
-def follow(request):
-    if request.method == 'POST':
-        follower = request.POST['follower']
-        user = request.POST['user']
-
-        if FollowersCount.objects.filter(follower=follower, user=user).first():
-            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
-            delete_follower.delete()
-            return redirect('/profile/'+user)
-        else:
-            new_follower = FollowersCount.objects.create(follower=follower, user=user)
-            new_follower.save()
-            return redirect('/profile/'+user)
-    else:
-        return redirect('/')
-
-@login_required(login_url='signin')
-def settings(request):
-    user_profile = Profile.objects.get(user=request.user)
-
-    if request.method == 'POST':
-        
-        if request.FILES.get('image') == None:
-            image = user_profile.profileimg
-            bio = request.POST['bio']
-            location = request.POST['location']
-
-            user_profile.profileimg = image
-            user_profile.bio = bio
-            user_profile.location = location
-            user_profile.save()
-        if request.FILES.get('image') != None:
-            image = request.FILES.get('image')
-            bio = request.POST['bio']
-            location = request.POST['location']
-
-            user_profile.profileimg = image
-            user_profile.bio = bio
-            user_profile.location = location
-            user_profile.save()
-        
-        return redirect('settings')
-    return render(request, 'setting.html', {'user_profile': user_profile})
-
-def signup(request):
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        password2 = request.POST['password2']
-
-        if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request, 'Email Taken')
-                return redirect('signup')
-            elif User.objects.filter(username=username).exists():
-                messages.info(request, 'Username Taken')
-                return redirect('signup')
-            else:
-                user = User.objects.create_user(username=username, email=email, password=password)
-                user.save()
-
-                #log user in and redirect to settings page
-                user_login = auth.authenticate(username=username, password=password)
-                auth.login(request, user_login)
-
-                #create a Profile object for the new user
-                user_model = User.objects.get(username=username)
-                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
-                new_profile.save()
-                return redirect('settings')
-        else:
-            messages.info(request, 'Password Not Matching')
-            return redirect('signup')
-        
-    else:
-        return render(request, 'signup.html')
-
-def signin(request):
-    
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(username=username, password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            return redirect('/')
-        else:
-            messages.info(request, 'Credentials Invalid')
-            return redirect('signin')
-
-    else:
-        return render(request, 'signin.html')
-
-@login_required(login_url='signin')
-def logout(request):
-    auth.logout(request)
-    return redirect('signin')
+def connections(request):
+  return render(request,'core/connections.html',{})
